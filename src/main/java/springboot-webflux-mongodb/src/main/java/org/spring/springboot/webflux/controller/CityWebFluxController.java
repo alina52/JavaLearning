9b0c12d3ecb961cf022d041c776bbf45739bace4
Alina Wang <alinawang1125@gmail.com>
2019-07-21 22:49:22 +0800

    springboot+mangodb

---
+++ src/main/java/springboot-webflux-mongodb/src/main/java/org/spring/springboot/webflux/controller/CityWebFluxController.java
@@ -0,0 +1,69 @@
+package org.spring.springboot.webflux.controller;
+
+import org.spring.springboot.domain.City;
+import org.spring.springboot.handler.CityHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Controller
+@RequestMapping(value = "/city")
+public class CityWebFluxController {
+
+    private static final String CITY_LIST_PATH_NAME = "cityList";
+    private static final String CITY_PATH_NAME = "city";
+
+    @Autowired
+    private CityHandler cityHandler;
+
+    @RequestMapping("/findById/{id}")
+    @ResponseBody
+    public Mono<City> findCityById(@PathVariable("id") Long id) {
+        return cityHandler.findCityById(id);
+    }
+
+    @GetMapping()
+    @ResponseBody
+    public Flux<City> findAllCity() {
+        return cityHandler.findAllCity();
+    }
+
+    @RequestMapping(value = "/insert", method = RequestMethod.POST)
+    @ResponseBody
+    public Mono<City> saveCity(City city) {
+        return cityHandler.save(city);
+    }
+
+    @PutMapping()
+    @ResponseBody
+    public Mono<City> modifyCity(@RequestBody City city) {
+        return cityHandler.modifyCity(city);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    @ResponseBody
+    public Mono<Long> deleteCity(@PathVariable("id") Long id) {
+        return cityHandler.deleteCity(id);
+    }
+
+    @GetMapping("/page/list")
+    public String listPage(final Model model) {
+        final Flux<City> cityFluxList = cityHandler.findAllCity();
+        model.addAttribute("cityList", cityFluxList);
+        return CITY_LIST_PATH_NAME;
+    }
+
+    @GetMapping("/getByName")
+    public String getByCityName(final Model model,
+                                @RequestParam("cityName") String cityName) {
+        final Mono<City> city = cityHandler.getByCityName(cityName);
+        model.addAttribute("city", city);
+        return CITY_PATH_NAME;
+    }
+
+
+
+}
